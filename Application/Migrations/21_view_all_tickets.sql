d6122c7f5be99172764d2c4a9450770d2f2d55e3
Peter Große <pegro@fem-net.de>
2013-12-07 15:35:31 +0100

    add view_all_tickets

---
+++ Application/Migrations/21_view_all_tickets.sql
@@ -0,0 +1,20 @@
+﻿BEGIN;
+
+SET search_path TO test;
+
+SET ROLE TO postgres;
+
+CREATE OR REPLACE VIEW view_all_tickets AS 
+	SELECT 
+		t.*, 
+		to_timestamp(ticket_fahrplan_starttime(t.id)) AS time_start, 
+		(to_timestamp(ticket_fahrplan_starttime(t.id)) + p.value::time)::timestamp AS time_end,
+		ticket_progress(t.id) AS ticket_progress
+	FROM
+		tbl_ticket t
+	LEFT JOIN
+		tbl_ticket_property p ON p.ticket_id = t.id AND p.name = 'Fahrplan.Duration'
+	ORDER BY
+		t.project_id ASC, time_start ASC, t.parent_id ASC;
+
+COMMIT;
