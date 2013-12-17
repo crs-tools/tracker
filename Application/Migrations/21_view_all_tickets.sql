99f982ca89a68815603e8d085ebfa90f815524dc
Peter Große <pegro@fem-net.de>
2013-12-17 02:00:01 +0100

    Model: DDL: add view_serviceable_tickets

--- Application/Migrations/21_view_all_tickets.sql
+++ Application/Migrations/21_view_all_tickets.sql
@@ -7,7 +7,8 @@ CREATE OR REPLACE VIEW view_all_tickets AS
 		t.*, 
 		to_timestamp(ticket_fahrplan_starttime(t.id)) AS time_start, 
 		(to_timestamp(ticket_fahrplan_starttime(t.id)) + p.value::time)::timestamp AS time_end,
-		ticket_progress(t.id) AS ticket_progress
+		ticket_progress(t.id) AS ticket_progress,
+		(SELECT tr.ticket_state FROM tbl_ticket tr WHERE tr.ticket_type = 'recording'::enum_ticket_type AND (tr.parent_id = t.id OR tr.parent_id = t.parent_id) LIMIT 1) AS recording_ticket_state
 	FROM
 		tbl_ticket t
 	LEFT JOIN
---
+++ Application/Migrations/22_view_serviceable_tickets.sql
@@ -0,0 +1,14 @@
+﻿BEGIN;
+
+SET ROLE TO postgres;
+
+CREATE OR REPLACE VIEW view_serviceable_tickets AS 
+	SELECT 
+		t.*, 
+		(SELECT ticket_state FROM ticket_state_next(t.project_id, t.ticket_type, t.ticket_state)) AS next_state,
+		(SELECT service_executable FROM ticket_state_next(t.project_id, t.ticket_type, t.ticket_state)) AS next_state_service_executable
+	FROM
+		tbl_ticket t
+	ORDER BY
+		ticket_priority(t.id) DESC;
+COMMIT;
