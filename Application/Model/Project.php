f6136943b38f34b9d622e328cbb731a0ac7e4755
Jannes Jeising <jannes@jeising.net>
2013-12-26 01:17:23 +0100

    Fixed issues with editing project encoding profile versions

--- Application/Model/Project.php
+++ Application/Model/Project.php
@@ -29,8 +29,8 @@
 				'foreign_key' => 'encoding_profile_version_id',
 				'self_key' => 'project_id',
 				'via' => 'tbl_project_encoding_profile',
-				// TODO: cleanup when Association has support
-				// 'select' => 'tbl_encoding_profile_version.revision, tbl_encoding_profile_version.created, tbl_encoding_profile_version.description, tbl_project_encoding_profile.priority'
+				// TODO: cleanup when Association has support (?)
+				'select' => 'tbl_encoding_profile_version.*, tbl_project_encoding_profile.priority'
 			),
 			// TODO: move to 'through' => 'ProjectState'
 			'States' => array(
@@ -52,6 +52,18 @@
 			'EncodingProfileVersion' => true,
 			'WorkerGroup' => true
 		);
+		
+		/*
+		public function getProgress($conditions = null, array $params = array()) {
+			$query = Database_Query::selectFrom($this->table, 'SUM(getTicketProgress(tbl_ticket.id)) / COUNT(tbl_ticket.id) AS progress', $conditions, $params);
+			$query->where('type_id = ? AND state_id != ?', array(1, $this->State->getIdByName('locked')));
+			
+			$this->Database->query($query);
+			$result = $this->Database->fetchRow();
+			
+			return (float) $result['progress'];
+		}
+		*/
 	}
 	
 ?>
