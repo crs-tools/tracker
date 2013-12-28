8b4bf12417bed926ae98b0b9d09233043ad463d3
Peter Große <pegro@fem-net.de>
2013-12-28 14:23:22 +0100

    Model: DDL: fixes

--- Application/Migrations/22_view_serviceable_tickets.sql
+++ Application/Migrations/22_view_serviceable_tickets.sql
@@ -7,10 +7,13 @@ CREATE OR REPLACE VIEW view_serviceable_tickets AS
 		t.*, 
 		to_timestamp(ticket_fahrplan_starttime(COALESCE(t.parent_id,t.id))) AS time_start,
 		(to_timestamp(ticket_fahrplan_starttime(COALESCE(t.parent_id,t.id))) + p.value::time)::timestamp AS time_end,
+    (SELECT value FROM tbl_ticket_property WHERE ticket_id = COALESCE(t.parent_id,t.id) AND name = 'Fahrplan.Room') as room,
 		(SELECT ticket_state FROM ticket_state_next(t.project_id, t.ticket_type, t.ticket_state)) AS next_state,
 		(SELECT service_executable FROM ticket_state_next(t.project_id, t.ticket_type, t.ticket_state)) AS next_state_service_executable
 	FROM
 		tbl_ticket t
-	ORDER BY
+  LEFT JOIN
+      tbl_ticket_property p ON p.ticket_id = COALESCE(t.parent_id,t.id) AND p.name = 'Fahrplan.Duration'
+  ORDER BY
 		ticket_priority(t.id) DESC;
 COMMIT;
