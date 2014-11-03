bee383dbe83cc72718fc08daf142ad9cdbba2cae
atze <atze@fem.tu-ilmenau.de>
2014-11-03 23:55:55 +0100

    begin #44, introduce function for initial ticket state for creation procedures

--- Application/Migrations/15_function_ticket_state.sql
+++ Application/Migrations/15_function_ticket_state.sql
@@ -58,4 +58,41 @@ END
 $$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION ticket_state_initial(param_project_id bigint, param_ticket_type enum_ticket_type)
+  RETURNS enum_ticket_state AS
+  $$
+DECLARE
+  ret enum_ticket_state;
+BEGIN
+  ret := 
+    (SELECT ts.ticket_state
+    FROM
+      tbl_ticket_state ts
+    JOIN
+      tbl_project_ticket_state pts ON pts.ticket_type = ts.ticket_type AND pts.ticket_state = ts.ticket_state
+    WHERE
+      pts.project_id = param_project_id AND
+      ts.ticket_type = param_ticket_type
+    ORDER BY ts.sort ASC
+    LIMIT 1);
+
+  IF ret IS NULL THEN
+    ret := (SELECT ts.ticket_state
+            FROM
+              tbl_ticket_state ts
+            WHERE
+              ts.sort = 0 AND
+              ts.ticket_type = param_ticket_type);
+  END IF;
+
+  IF ret IS NULL THEN
+    RAISE WARNING 'No default ticket state for project!';
+    ret := NULL::enum_ticket_state;
+  END IF;
+
+  RETURN ret;
+END
+$$
+LANGUAGE plpgsql;
+
 COMMIT;
