81621afb0c4d21da25131404f1e3bc23a6e061c8
atze <atze@fem.tu-ilmenau.de>
2014-11-16 13:50:01 +0100

    bugfix migration, add table and migration for #96

--- Application/Migrations/04_projects.sql
+++ Application/Migrations/04_projects.sql
@@ -70,6 +70,22 @@ CREATE TABLE tbl_project_worker_group
 )
 WITHOUT OIDS;
 
+CREATE TABLE tbl_project_worker_group_filter
+(
+  project_id bigint NOT NULL,
+  worker_group_id bigint NOT NULL,
+  property_key ltree NOT NULL CHECK (char_length(property_key::text) > 0),
+  property_value ltree NOT NULL CHECK (char_length(property_value::text) > 0),
+  CONSTRAINT tbl_project_worker_group_filter_pk PRIMARY KEY (project_id, worker_group_id, property_key, property_value),
+  CONSTRAINT tbl_project_worker_group_filter_group_fk FOREIGN KEY (worker_group_id)
+      REFERENCES tbl_worker_group (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT tbl_project_worker_group_filter_project_fk FOREIGN KEY (project_id)
+      REFERENCES tbl_project (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITHOUT OIDS;
+
 CREATE TABLE tbl_project_encoding_profile
 (
   project_id bigint NOT NULL,
--- Application/Migrations/__tmp_upgrade_tables-2014-11-13.sql
+++ Application/Migrations/__tmp_upgrade_tables-2014-11-13.sql
@@ -3,15 +3,32 @@ BEGIN;
 UPDATE tbl_ticket SET title = NULL WHERE parent_id IS NOT NULL;
 
 ALTER TABLE "public"."tbl_comment" ADD COLUMN "referenced_ticket_id" bigint;
-ALTER TABLE "public"."tbl_comment" ADD CONSTRAINT "tbl_comment_log_fkt_referenced_ticket_fk" FOREIGN KEY ("referenced_ticket_id") REFERENCES "public"."tbl_ticket" ("id") ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE "public"."tbl_comment" DROP CONSTRAINT IF EXISTS "tbl_comment_log_fkt_referenced_ticket_fk";
 ALTER TABLE "public"."tbl_comment" ADD CONSTRAINT "tbl_comment_log_fkt_referenced_ticket_fk" FOREIGN KEY ("referenced_ticket_id") REFERENCES tbl_ticket (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE SET NULL;
 
-# Move comments from children to parent, set reference
+-- Move comments from children to parent, set reference
 UPDATE tbl_comment SET referenced_ticket_id = ticket_id, ticket_id = (SELECT parent_id FROM tbl_ticket WHERE tbl_ticket.id = tbl_comment.ticket_id)
 WHERE id IN (
 	SELECT tbl_comment.id FROM tbl_comment
 	INNER JOIN tbl_ticket ON tbl_comment.ticket_id = tbl_ticket.id
 	WHERE tbl_ticket.parent_id IS NOT NULL
 );
+
+CREATE TABLE tbl_project_worker_group_filter
+(
+  project_id bigint NOT NULL,
+  worker_group_id bigint NOT NULL,
+  property_key ltree NOT NULL CHECK (char_length(property_key::text) > 0),
+  property_value ltree NOT NULL CHECK (char_length(property_value::text) > 0),
+  CONSTRAINT tbl_project_worker_group_filter_pk PRIMARY KEY (project_id, worker_group_id, property_key, property_value),
+  CONSTRAINT tbl_project_worker_group_filter_group_fk FOREIGN KEY (worker_group_id)
+      REFERENCES tbl_worker_group (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT tbl_project_worker_group_filter_project_fk FOREIGN KEY (project_id)
+      REFERENCES tbl_project (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITHOUT OIDS;
+
 
 COMMIT;
