62d1f27a87d6001865dd3a12c43dd2f65918e980
atze <atze@fem.tu-ilmenau.de>
2014-11-22 14:36:19 +0100

    consider staged-status of meta ticket in automated state changes on encoding tickets

--- Application/Migrations/05_tickets.sql
+++ Application/Migrations/05_tickets.sql
@@ -158,7 +158,13 @@ CREATE TRIGGER inherit_fahrplan_id BEFORE INSERT OR UPDATE ON tbl_ticket FOR EAC
 -- update encoding tickets state to "ready to encode", if recording ticket changes state to "finalized"
 CREATE OR REPLACE FUNCTION update_encoding_ticket_state() RETURNS trigger AS $$
 BEGIN
-  IF NEW.ticket_type = 'recording' AND NEW.ticket_state <> OLD.ticket_state AND NEW.ticket_state = 'finalized' THEN
+  IF NEW.ticket_type = 'recording' 
+    AND NEW.ticket_state <> OLD.ticket_state 
+    AND NEW.ticket_state = 'finalized' 
+    AND 'staged' = COALESCE(
+      (SELECT ticket_state FROM tbl_ticket tt WHERE tt.ticket_type='meta' AND tt.id=NEW.parent_id),
+      'staging') 
+    THEN
     UPDATE tbl_ticket SET ticket_state = 'ready to encode' WHERE ticket_type = 'encoding' AND parent_id = NEW.parent_id AND ticket_state = 'material needed';
   END IF;
   RETURN NEW;
@@ -170,9 +176,14 @@ CREATE TRIGGER update_encoding_ticket_state AFTER UPDATE ON tbl_ticket FOR EACH 
 -- set inserted encoding tickets state to "ready to encode", if corresponding recording ticket is in "finalized"
 CREATE OR REPLACE FUNCTION set_encoding_ticket_state() RETURNS trigger AS $$
 BEGIN
-  IF NEW.ticket_type = 'encoding' AND 'finalized' = COALESCE(
-    (SELECT ticket_state FROM tbl_ticket tt WHERE tt.ticket_type='recording' AND tt.parent_id=NEW.parent_id),
-    'scheduled') THEN
+  IF NEW.ticket_type = 'encoding' 
+    AND 'finalized' = COALESCE(
+      (SELECT ticket_state FROM tbl_ticket tt WHERE tt.ticket_type='recording' AND tt.parent_id=NEW.parent_id),
+      'scheduled') 
+    AND 'staged' = COALESCE(
+      (SELECT ticket_state FROM tbl_ticket tt WHERE tt.ticket_type='meta' AND tt.id=NEW.parent_id),
+      'staging') 
+    THEN
     NEW.ticket_state := 'ready to encode';
   END IF;
   RETURN NEW;
