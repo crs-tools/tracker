6a2d8d9d397784bf0206176559a0cabc016abd9a
Jannes Jeising <jannes@jeising.net>
2015-10-08 10:38:36 +0200

    Update Codemirror

--- Application/View/shared/js/_editor.html.php
+++ Application/View/shared/js/_editor.html.php
@@ -1,4 +1,4 @@
 <?php
 	$this->contentFor('stylesheets', '<link rel="stylesheet" href="' . $this->Request->getRootURL() . 'css/codemirror.css" type="text/css" />');
-	$this->contentFor('scripts', '<script src="' . $this->Request->getRootURL() . 'javascript/codemirror-4.9.js" type="text/javascript"></script>');
+	$this->contentFor('scripts', '<script src="' . $this->Request->getRootURL() . 'javascript/codemirror-5.7.js" type="text/javascript"></script>');
 ?>
