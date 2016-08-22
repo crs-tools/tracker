ffc4970fcf24a6a482c3dd19f985a7f8377bb791
atze <atze@fem.tu-ilmenau.de>
2016-08-22 17:01:49 +0200

    new DB structure for enhanced filter. WARNING migration truncates tables

--- Application/Migrations/04_projects.sql
+++ Application/Migrations/04_projects.sql
@@ -60,7 +60,11 @@ CREATE TABLE tbl_project_worker_group
 (
   project_id bigint NOT NULL,
   worker_group_id bigint NOT NULL,
-  CONSTRAINT tbl_project_worker_group_pk PRIMARY KEY (project_id, worker_group_id),
+  ticket_type enum_ticket_type NOT NULL,
+  ticket_state enum_ticket_state NOT NULL,
+  CONSTRAINT tbl_project_worker_group_pk 
+      PRIMARY KEY (project_id, worker_group_id,
+      ticket_type, ticket_state),
   CONSTRAINT tbl_project_worker_group_group_fk FOREIGN KEY (worker_group_id)
       REFERENCES tbl_worker_group (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE,
@@ -75,9 +79,13 @@ CREATE TABLE tbl_project_worker_group_filter
   id bigserial NOT NULL,
   project_id bigint NOT NULL,
   worker_group_id bigint NOT NULL,
+  ticket_type enum_ticket_type NOT NULL,
+  ticket_state enum_ticket_state NOT NULL,
   property_key ltree NOT NULL CHECK (char_length(property_key::text) > 0),
   property_value character varying(8196) NOT NULL,
-  CONSTRAINT tbl_project_worker_group_filter_pk PRIMARY KEY (id, project_id, worker_group_id, property_key),
+  CONSTRAINT tbl_project_worker_group_filter_pk 
+      PRIMARY KEY (id, project_id, worker_group_id, 
+      ticket_type, ticket_state, property_key),
   CONSTRAINT tbl_project_worker_group_filter_group_fk FOREIGN KEY (worker_group_id)
       REFERENCES tbl_worker_group (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE,
