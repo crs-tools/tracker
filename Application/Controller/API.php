b557b95348e61db342681637e7101f4b45f89999
Jannes Jeising <jannes@jeising.net>
2016-12-28 13:15:26 +0100

    More hacks for API language support

--- Application/Controller/API.php
+++ Application/Controller/API.php
@@ -52,7 +52,8 @@
 						'Fahrplan.GUID' => 'fahrplan_guid',
 						'Record.Cutdiffseconds' => 'duration',
 						'YouTube.Url0' => 'youtube_url',
-						'YouTube.Url1' => 'youtube_url_translated'
+						'YouTube.Url1' => 'youtube_url_translated',
+						'YouTube.Url2' => 'youtube_url_translated_2'
 					]]
 				]);
 			
