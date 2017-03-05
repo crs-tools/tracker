950acbbf9c75ecf02cfa214d43b8448eec26b56a
atze <atze@fem.tu-ilmenau.de>
2017-03-05 22:17:38 +0100

    fix whitespaces / intendation

--- Application/Migrations/14_function_create_missing_recording_tickets.sql
+++ Application/Migrations/14_function_create_missing_recording_tickets.sql
@@ -8,23 +8,28 @@ CREATE OR REPLACE FUNCTION create_missing_recording_tickets(param_project_id big
   BEGIN
 	row_count := 0;
 
-	 INSERT INTO tbl_ticket (parent_id, project_id, fahrplan_id, ticket_type, ticket_state)
+	INSERT INTO tbl_ticket (parent_id, project_id, fahrplan_id, ticket_type, ticket_state)
 		(SELECT
-       t1.id as parent_id,
-       t1.project_id,
-       t1.fahrplan_id,
-       'recording' as ticket_type,
-       ticket_state_initial(param_project_id, 'recording') AS ticket_state
-     FROM
-         tbl_ticket t1
-         LEFT JOIN
-         tbl_ticket t2 ON t2.parent_id = t1.id AND t2.ticket_type = 'recording'
-     WHERE
-       t1.ticket_type = 'meta' AND
-       t1.project_id = param_project_id
-     GROUP BY t1.id HAVING COUNT(t2.id) = 0);
-		GET DIAGNOSTICS row_count = ROW_COUNT;
-	  return row_count;
+			t1.id as parent_id,
+			t1.project_id,
+			t1.fahrplan_id,
+			'recording' as ticket_type,
+			ticket_state_initial(param_project_id, 'recording') AS ticket_state
+		FROM
+			tbl_ticket t1
+		LEFT JOIN
+			tbl_ticket t2 ON t2.parent_id = t1.id AND t2.ticket_type = 'recording'
+		WHERE
+			t1.ticket_type = 'meta' AND
+			t1.project_id = param_project_id
+		GROUP BY 
+			t1.id 
+		HAVING COUNT(t2.id) = 0
+		)
+	;
+
+	GET DIAGNOSTICS row_count = ROW_COUNT;
+	RETURN row_count;
   END;
 $$ LANGUAGE plpgsql;
 
