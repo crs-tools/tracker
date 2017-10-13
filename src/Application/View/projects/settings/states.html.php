46c79173ab88e8b8b47526ab154e985a9dc1de85
Markus Otto <otto@fs.tum.de>
2017-10-13 12:40:31 +0200

    center some column titles in state settings view

--- src/Application/View/projects/settings/states.html.php
+++ src/Application/View/projects/settings/states.html.php
@@ -34,8 +34,8 @@ $typeRows = 0; ?>
                 <tr>
                     <th width="20%">Type</th>
                     <th width="40%">State</th>
-                    <th width="15%" title="<?=$title_service?>">Service</th>
-                    <th width="15%" title="<?=$title_skip?>">Master only</th>
+                    <th width="15%" class="center" title="<?=$title_service?>">Service</th>
+                    <th width="15%" class="center" title="<?=$title_skip?>">Master only</th>
                 </tr>
             </thead>
             <tbody>
