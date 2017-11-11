03edd8482f98d9a95e60fd55fdac4073b948475f
atze <atze@fem.tu-ilmenau.de>
2017-11-11 21:42:02 +0100

    fix order of table creation

--- src/Application/Migrations/04_projects.sql
+++ src/Application/Migrations/04_projects.sql
@@ -103,4 +103,19 @@ CREATE TABLE tbl_project_encoding_profile
 )
 WITHOUT OIDS;
 
+CREATE TABLE tbl_user_project_restrictions
+(
+  user_id bigint NOT NULL,
+  project_id bigint NOT NULL,
+  role character varying(32) DEFAULT 'user'::character varying,
+  CONSTRAINT tbl_user_project_restrictions_pk PRIMARY KEY (user_id, project_id),
+  CONSTRAINT tbl_user_project_restrictions_user_fk FOREIGN KEY (user_id)
+      REFERENCES tbl_user (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT tbl_user_project_restrictions_project_fk FOREIGN KEY (project_id)
+      REFERENCES tbl_project (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITHOUT OIDS;
+
 COMMIT;
