25d072700d2725f1060bc0ddefd8128fd0a7448f
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2018-04-15 03:03:49 +0200

    use per-project trigger state in ticket view, related #157

---
+++ src/Application/Migrations/17_function_ticket_dependency_satisfied.sql
@@ -0,0 +1,43 @@
+﻿BEGIN;
+
+SET ROLE TO postgres;
+
+CREATE OR REPLACE FUNCTION ticket_depending_encoding_ticket_state_satisfied(param_ticket_id bigint)
+  RETURNS boolean AS
+  $$
+DECLARE
+	state enum_ticket_state;
+	satisfaction boolean;
+BEGIN
+	SELECT
+		t2.ticket_state INTO state
+	FROM
+		tbl_ticket t
+	JOIN
+		tbl_encoding_profile_version epv ON epv.id = t.encoding_profile_version_id
+	JOIN
+		tbl_encoding_profile ep ON epv.encoding_profile_id = ep.id
+	JOIN
+		tbl_encoding_profile ep2 ON ep2.id = ep.depends_on
+	JOIN
+		tbl_encoding_profile_version epv2 ON epv2.encoding_profile_id = ep2.id
+	JOIN
+		tbl_ticket t2 ON t2.encoding_profile_version_id = epv2.id AND t2.parent_id = t.parent_id
+	WHERE
+		t.id = param_ticket_id;
+
+	SELECT
+		state >= p.subticket_trigger_state INTO satisfaction
+	FROM
+		tbl_ticket t
+	JOIN
+		tbl_project p ON t.project_id = p.id
+	WHERE
+		t.id = param_ticket_id;
+
+	RETURN satisfaction;
+END
+$$
+LANGUAGE plpgsql;
+
+COMMIT;
