154cee0e53f4418a68adda29484212ab1dcf3962
atze <atze@fem.tu-ilmenau.de>
2018-04-18 19:51:20 +0200

    add new DB migration, completely supersedes last one

--- src/Application/Migrations/__2018-04-18_add_variable_dependent_ticket_state.sql
+++ src/Application/Migrations/__2018-04-18_add_variable_dependent_ticket_state.sql
@@ -3,17 +3,17 @@ BEGIN;
 SET ROLE TO postgres;
 
 ALTER TABLE tbl_project
-  ADD COLUMN dependent_ticket_trigger_state enum_ticket_state
+  ADD COLUMN 
+  IF NOT EXISTS -- NOTE: this works only with Postgres >= 9.6
+  dependent_ticket_trigger_state enum_ticket_state
   NOT NULL DEFAULT 'released';
-
-DROP VIEW IF EXISTS view_serviceable_tickets;
 
 CREATE OR REPLACE VIEW view_serviceable_tickets AS 
 	SELECT 
 		t.*,
 		pstart.value::timestamp with time zone AS time_start,
 		pstart.value::timestamp with time zone + pdur.value::time without time zone::interval AS time_end,
-		(SELECT value FROM tbl_ticket_property WHERE ticket_id = COALESCE(t.parent_id,t.id) AND name = 'Fahrplan.Room') as room,
+		proom.value as room,
 		t.ticket_state_next AS next_state,
 		t.service_executable AS next_state_service_executable
 	FROM
@@ -25,16 +25,29 @@ CREATE OR REPLACE VIEW view_serviceable_tickets AS
 	LEFT JOIN 
 		tbl_ticket_property pstart ON pstart.ticket_id = COALESCE(t.parent_id, t.id) AND pstart.name = 'Fahrplan.DateTime'::ltree
 	LEFT JOIN
+		tbl_ticket_property proom ON proom.ticket_id = COALESCE(t.parent_id,t.id) AND proom.name = 'Fahrplan.Room'::ltree
+	LEFT JOIN
 		tbl_project pj ON pj.id = t.project_id
-	LEFT JOIN tbl_project_encoding_profile pep ON pep.project_id = pj.id AND pep.encoding_profile_version_id = t.encoding_profile_version_id
+	LEFT JOIN
+		tbl_project_encoding_profile pep ON pep.project_id = pj.id AND pep.encoding_profile_version_id = t.encoding_profile_version_id
+	LEFT JOIN
+		tbl_ticket_state state ON state.ticket_type = t.ticket_type AND state.ticket_state = t.ticket_state
+	LEFT JOIN
+		tbl_ticket_state wantedstate ON wantedstate.ticket_type = 'encoding' AND wantedstate.ticket_state = pj.dependent_ticket_trigger_state
+	LEFT JOIN
+		tbl_ticket_state masterstate ON
+			masterstate.ticket_type = 'encoding' AND
+			masterstate.ticket_state = COALESCE(ticket_depending_encoding_ticket_state(t.id),pj.dependent_ticket_trigger_state)
 
 	WHERE
+		pj.read_only = false AND
+		t.service_executable = true AND
 		t.ticket_type != 'meta' AND
 		pt.ticket_state = 'staged' AND
 		pt.failed = false AND
 		t.failed = false AND
 		COALESCE(pep.priority, 1) > 0 AND
-		COALESCE(ticket_depending_encoding_ticket_state(t.id),pj.dependent_ticket_trigger_state) >= pj.dependent_ticket_trigger_state
+		masterstate.sort >= wantedstate.sort
 	ORDER BY
 		ticket_priority(t.id) DESC;
 
@@ -63,11 +76,15 @@ BEGIN
 		t.id = param_ticket_id;
 
 	SELECT
-		state >= p.dependent_ticket_trigger_state INTO satisfaction
+		s1.sort >= s2.sort INTO satisfaction
 	FROM
 		tbl_ticket t
 	JOIN
 		tbl_project p ON t.project_id = p.id
+	JOIN
+		tbl_ticket_state s1 ON t.ticket_type = 'encoding' AND t.ticket_state = s1.ticket_state
+	JOIN
+		tbl_ticket_state s2 ON t.ticket_type = 'encoding' AND p.dependent_ticket_trigger_state = s2.ticket_state
 	WHERE
 		t.id = param_ticket_id;
 
@@ -79,8 +96,8 @@ LANGUAGE plpgsql;
 COMMIT;
 
 
-
 DO language plpgsql $$
+
 BEGIN
   RAISE WARNING 'DO NOT FORGET: it is very likely that you have to give GRANT on the recreated view_serviceable_tickets to your tracker DB user after executing this script!';
 END
