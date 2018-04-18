42aaabb63645939da9d0b74ba6cddd27bf9d04eb
atze <atze@fem.tu-ilmenau.de>
2018-04-18 19:50:31 +0200

    SQL: optimize view, change subselect to join for room property

--- src/Application/Migrations/22_view_serviceable_tickets.sql
+++ src/Application/Migrations/22_view_serviceable_tickets.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE VIEW view_serviceable_tickets AS
 		t.*,
 		pstart.value::timestamp with time zone AS time_start,
 		pstart.value::timestamp with time zone + pdur.value::time without time zone::interval AS time_end,
-		(SELECT value FROM tbl_ticket_property WHERE ticket_id = COALESCE(t.parent_id,t.id) AND name = 'Fahrplan.Room') as room,
+		proom.value as room,
 		t.ticket_state_next AS next_state,
 		t.service_executable AS next_state_service_executable
 	FROM
@@ -20,6 +20,8 @@ CREATE OR REPLACE VIEW view_serviceable_tickets AS
 		tbl_ticket_property pdur ON pdur.ticket_id = COALESCE(t.parent_id, t.id) AND pdur.name = 'Fahrplan.Duration'::ltree
 	LEFT JOIN 
 		tbl_ticket_property pstart ON pstart.ticket_id = COALESCE(t.parent_id, t.id) AND pstart.name = 'Fahrplan.DateTime'::ltree
+	LEFT JOIN
+		tbl_ticket_property proom ON proom.ticket_id = COALESCE(t.parent_id,t.id) AND proom.name = 'Fahrplan.Room'::ltree
 	LEFT JOIN
 		tbl_project pj ON pj.id = t.project_id
 	LEFT JOIN tbl_project_encoding_profile pep ON pep.project_id = pj.id AND pep.encoding_profile_version_id = t.encoding_profile_version_id
