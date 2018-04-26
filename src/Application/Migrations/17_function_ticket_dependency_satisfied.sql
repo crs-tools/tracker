0232dff0f9aecf8f6c21cb8b635dcf8169016e97
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2018-04-27 00:46:58 +0200

    SQL: clarify table aliases

--- src/Application/Migrations/17_function_ticket_dependency_satisfied.sql
+++ src/Application/Migrations/17_function_ticket_dependency_satisfied.sql
@@ -11,20 +11,20 @@ BEGIN
 	SELECT
 		dependent_ticket_state.sort >= configured_trigger_state.sort INTO satisfaction
 	FROM
-		tbl_ticket t
+		tbl_ticket param_ticket
 	JOIN
-		tbl_project p ON t.project_id = p.id
+		tbl_project project ON param_ticket.project_id = project.id
 	JOIN
 		-- if given ticket is no encoding ticket, function will return NULL
 		tbl_ticket_state dependent_ticket_state ON
-			t.ticket_type = 'encoding' AND
-			dependent_ticket_state.ticket_state = t.ticket_state
+			param_ticket.ticket_type = 'encoding' AND
+			dependent_ticket_state.ticket_state = param_ticket.ticket_state
 	JOIN
 		tbl_ticket_state configured_trigger_state ON
-			t.ticket_type = 'encoding' AND
-			configured_trigger_state.ticket_state = p.dependent_ticket_trigger_state
+			param_ticket.ticket_type = 'encoding' AND
+			configured_trigger_state.ticket_state = project.dependent_ticket_trigger_state
 	WHERE
-		t.id = param_ticket_id;
+		param_ticket.id = param_ticket_id;
 
 	RETURN satisfaction;
 END
