f265604d74f3b825c41959ad24e921bd860bbf6d
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2018-04-27 00:44:35 +0200

    SQL: remove unused part of function

--- src/Application/Migrations/17_function_ticket_dependency_satisfied.sql
+++ src/Application/Migrations/17_function_ticket_dependency_satisfied.sql
@@ -6,26 +6,8 @@ CREATE OR REPLACE FUNCTION ticket_depending_encoding_ticket_state_satisfied(para
   RETURNS boolean AS
   $$
 DECLARE
-	state enum_ticket_state;
 	satisfaction boolean;
 BEGIN
-	SELECT
-		t2.ticket_state INTO state
-	FROM
-		tbl_ticket t
-	JOIN
-		tbl_encoding_profile_version epv ON epv.id = t.encoding_profile_version_id
-	JOIN
-		tbl_encoding_profile ep ON epv.encoding_profile_id = ep.id
-	JOIN
-		tbl_encoding_profile ep2 ON ep2.id = ep.depends_on
-	JOIN
-		tbl_encoding_profile_version epv2 ON epv2.encoding_profile_id = ep2.id
-	JOIN
-		tbl_ticket t2 ON t2.encoding_profile_version_id = epv2.id AND t2.parent_id = t.parent_id
-	WHERE
-		t.id = param_ticket_id;
-
 	SELECT
 		dependent_ticket_state.sort >= configured_trigger_state.sort INTO satisfaction
 	FROM
