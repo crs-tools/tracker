8ea748ad2e96f0b7e3a67d773900b888c00d9588
Jonas Licht <jonas.licht@fem.tu-ilmenau.de>
2019-02-21 21:08:26 +0100

    SQL: add function ticket reset
    This function resets the given ticket and all tickets depends on the given ticket.

--- src/Application/Migrations/15_function_ticket_state.sql
+++ src/Application/Migrations/15_function_ticket_state.sql
@@ -129,4 +129,90 @@ END
 $$
 LANGUAGE plpgsql;
 
+-- function also resets handle_id and failed flag
+CREATE OR REPLACE FUNCTION ticket_reset(param_ticket_id bigint, param_ticket_types enum_ticket_type[] DEFAULT array[]::enum_ticket_type[])
+  RETURNS TABLE (ticket_id bigint,  from_state enum_ticket_state, to_state enum_ticket_state, follows_meta_ticket boolean, follows_encoding_ticket boolean) AS
+$$
+DECLARE
+  ticket tbl_ticket%rowtype;
+  inital_state enum_ticket_state;
+BEGIN
+  --first get the full given ticket
+  SELECT * INTO ticket FROM tbl_ticket where id = param_ticket_id;
+
+  IF NOT FOUND THEN
+    RAISE WARNING 'ticket with id % does not exist', param_ticket_id;
+    RETURN;
+  END IF;
+
+  --reset the given ticket to inital state, but only if it matches the ticket type filter
+  IF array_length(param_ticket_types, 1) IS NULL OR (array_position(param_ticket_types, ticket.ticket_type) IS NOT NULL) THEN
+    inital_state := ticket_state_initial(ticket.project_id, ticket.ticket_type);
+    UPDATE tbl_ticket SET (failed, handle_id) = (FALSE, NULL) WHERE id = ticket.id;
+    IF inital_state <> ticket.ticket_state THEN
+      UPDATE tbl_ticket SET ticket_state=inital_state WHERE id = param_ticket_id;
+      ticket_id := ticket.id;
+      from_state := ticket.ticket_state;
+      to_state := inital_state;
+      follows_meta_ticket := FALSE;
+      follows_encoding_ticket := FALSE;
+      RETURN NEXT;
+    END IF;
+  END IF;
+
+  --reset the children of the given ticket
+  FOR ticket IN
+  SELECT * FROM tbl_ticket WHERE parent_id = param_ticket_id AND (array_length(param_ticket_types, 1) IS NULL OR (array_position(param_ticket_types, ticket_type) IS NOT NULL))
+  LOOP
+    inital_state := ticket_state_initial(ticket.project_id, ticket.ticket_type);
+    UPDATE tbl_ticket SET (failed, handle_id) = (FALSE, NULL) WHERE id = ticket.id;
+    IF inital_state <> ticket.ticket_state THEN
+      UPDATE tbl_ticket SET ticket_state = inital_state WHERE id = ticket.id;
+      ticket_id := ticket.id;
+      from_state := ticket.ticket_state;
+      to_state := inital_state;
+      follows_meta_ticket := TRUE;
+      follows_encoding_ticket := FALSE;
+      RETURN NEXT;
+    END IF;
+  END LOOP;
+
+  --reset all tickets with encoding profiles depending on given ticket's profile
+  FOR ticket IN
+    SELECT
+      t2.*
+    FROM
+      tbl_ticket t2
+    JOIN
+      tbl_encoding_profile_version epv2 ON t2.encoding_profile_version_id = epv2.id
+    JOIN (
+      SELECT
+        encoding_profile_all_dependees(epv.encoding_profile_id) AS id,
+        tt.parent_id
+      FROM
+        tbl_ticket tt
+      JOIN
+        tbl_encoding_profile_version epv ON tt.encoding_profile_version_id = epv.id
+      WHERE
+        tt.id = param_ticket_id AND
+        (array_length(param_ticket_types, 1) IS NULL OR (array_position(param_ticket_types, tt.ticket_type) IS NOT NULL))
+    ) pd ON pd.id = epv2.encoding_profile_id AND t2.parent_id = pd.parent_id
+  LOOP
+    inital_state := ticket_state_initial(ticket.project_id, ticket.ticket_type);
+    UPDATE tbl_ticket SET (failed, handle_id) = (FALSE, NULL) WHERE id = ticket.id;
+    IF inital_state <> ticket.ticket_state THEN
+      UPDATE tbl_ticket SET ticket_state = inital_state WHERE id = ticket.id;
+      ticket_id := ticket.id;
+      from_state := ticket.ticket_state;
+      to_state := inital_state;
+      follows_meta_ticket := FALSE;
+      follows_encoding_ticket := TRUE;
+      RETURN NEXT;
+    END IF;
+  END LOOP;
+
+END
+$$
+LANGUAGE plpgsql;
+
 COMMIT;
