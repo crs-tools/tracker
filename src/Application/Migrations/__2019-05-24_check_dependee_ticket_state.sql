047a8545dd6b2bce096c7e41ed8fb96e37ce18d1
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2019-05-24 12:32:19 +0200

    Add condition to check that depending ticket does not progress more than dependee ticket.
    Fixes #208

--- src/Application/Migrations/__2019-05-24_check_dependee_ticket_state.sql
+++ src/Application/Migrations/__2019-05-24_check_dependee_ticket_state.sql
@@ -80,3 +80,9 @@ CREATE OR REPLACE VIEW view_serviceable_tickets AS
 		calculated_priority DESC;
 
 COMMIT;
+DO language plpgsql $$
+BEGIN
+  RAISE WARNING 'DO NOT FORGET: it is very likely that you have to give GRANT on the recreated view_serviceable_tickets to your tracker DB user after executing this script!';
+END
+$$;
+
