c31602918442c73b509c6de251c0caba411ac784
Matthias Hunstock <atze@fem.tu-ilmenau.de>
2019-05-24 12:06:52 +0200

    Fix function definition, inner join is necessary as soon as there are multipe profile versions available.
    Fixes #199

--- src/Application/Migrations/__2019-05-24_fix-dependee-ticket-missing.sql
+++ src/Application/Migrations/__2019-05-24_fix-dependee-ticket-missing.sql
@@ -24,7 +24,7 @@ BEGIN
 		tbl_encoding_profile ep2 ON ep2.id = ep.depends_on
 	LEFT JOIN
 		tbl_encoding_profile_version epv2 ON epv2.encoding_profile_id = ep2.id
-	LEFT JOIN
+	INNER JOIN
 		tbl_ticket dependee ON dependee.encoding_profile_version_id = epv2.id AND dependee.parent_id = depender.parent_id
 	WHERE
 		depender.id = param_depender_ticket_id;
